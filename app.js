436011fd3544ec7e98f7ef8ef3551ad1fef44c7c
DongHyun <pca468@naver.com>
2024-10-23 01:27:23 +0900

    please deploy

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+console.log("mongoURI", MONGODB_URI_PROD);
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter); // /api를 해주면 각 주소 앞에 /api가 붙는다 ex) /api/tasks 블라블라
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
@@ -22,7 +26,6 @@ mongoose
   });
 // useNewUrlParser 은  mongodb 형태가 약간 다르다 옛날과 요즘 형식이 다른 부분을 요즘 형식도 잘 나올 수 있게 해달라는 코드
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server on 5000");
 });
-
